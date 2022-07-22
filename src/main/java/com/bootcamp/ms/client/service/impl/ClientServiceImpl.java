54492682cdd0e2b1e43816657c40c83a39d05bea
Rivero13 <rivero_jair@hotmail.com>
2022-07-22 12:03:53 -0500

    pruebas con mockito

--- src/main/java/com/bootcamp/ms/client/service/impl/ClientServiceImpl.java
+++ src/main/java/com/bootcamp/ms/client/service/impl/ClientServiceImpl.java
@@ -3,18 +3,24 @@ package com.bootcamp.ms.client.service.impl;
 import com.bootcamp.ms.commons.entity.Client;
 import com.bootcamp.ms.client.repository.ClientRepository;
 import com.bootcamp.ms.client.service.ClientService;
+import com.bootcamp.ms.commons.entity.ProductBank;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ClientServiceImpl implements ClientService {
 
   @Autowired
   private ClientRepository clientRepository;
+
+  public ClientServiceImpl(ClientRepository clientRepository) {
+    this.clientRepository = clientRepository;
+  }
 
   @Override
   public Flux<Client> findAll() {
@@ -34,5 +40,13 @@ public class ClientServiceImpl implements ClientService {
   @Override
   public Mono<Void> delete(Client client) {
     return clientRepository.delete(client);
+  }
+
+  @Override
+  public Optional<Client> findByDocumentNumber(String description) {
+    return clientRepository.findAll()
+            .toStream()
+            .filter(c -> c.getDocumentNumber().contains(description))
+            .findFirst();
   }
 }
